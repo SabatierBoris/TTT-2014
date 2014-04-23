6a35ea56c01951f39137b8022da4605f77c447e3
SABATIER Boris <sabatier.boris@gmail.com>
2014-04-23 16:12:56 +0200

    Mise en place de l'asservisement Tank

--- libNXT/src/TTT/libNXT/navigation/TankAsservise.java
+++ libNXT/src/TTT/libNXT/navigation/TankAsservise.java
@@ -11,6 +11,25 @@ public class TankAsservise extends AbstractAsservise{
 
 	@Override
 	protected void speedsUpdate(int linearSpeed, int angularSpeed){
+		int M1Speed=0;
+		int M2Speed=0;
+
+		if(this.linearIsLock()){
+			M1Speed += linearSpeed;
+			M2Speed += linearSpeed;
+		}
+		if(this.angularIsLock()){
+			M1Speed += angularSpeed;
+			M2Speed -= angularSpeed;
+		}
+
+		if(this.linearIsLock() || this.angularIsLock()){
+			this.setM1Float(false);
+			this.setM2Float(false);
+		}else{
+			this.setM1Float(true);
+			this.setM2Float(true);
+		}
 		this.setM1Speed(linearSpeed+angularSpeed);
 		this.setM2Speed(linearSpeed-angularSpeed);
 	}
