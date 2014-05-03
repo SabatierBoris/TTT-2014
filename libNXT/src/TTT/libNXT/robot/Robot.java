2910a27925c8e071ff6823ac64e1f9e2965f87d3
SABATIER Boris <sabatier.boris@gmail.com>
2014-05-03 16:10:49 +0200

    Abstracisation du model de Robot

--- libNXT/src/TTT/libNXT/robot/Robot.java
+++ libNXT/src/TTT/libNXT/robot/Robot.java
@@ -2,19 +2,20 @@ package TTT.libNXT.robot;
 
 import java.util.ArrayList;
 
-import lejos.robotics.Touch;
 import lejos.nxt.LCD;
 
-public class Robot {
-	private static final long TIME_LIMIT = 90000l; //TODO fix
+public abstract class Robot {
 	private ArrayList<Thread> tasks;
-	private Touch starter;
+	private long timeLimit;
 
-	public Robot(Touch starter){
+	public Robot(long timeLimit){
 		this.tasks = new ArrayList<Thread>();
-		this.starter = starter;
+		this.timeLimit = timeLimit;
 	}
 
+	abstract public void insertStarter();
+	abstract public void removeStarter();
+	abstract public boolean ARUIsPress();
 
 	public void addTask(Thread t){
 		this.tasks.add(t);
@@ -26,28 +27,24 @@ public class Robot {
 			t.setDaemon(true);
 			t.start();
 		}
-		System.out.println("INSERT STARTER");
-		while(!this.starter.isPressed()){
-			Thread.yield();
-		}
-		System.out.println("REMOVE STARTER");
-		while(this.starter.isPressed()){
-			Thread.yield();
-		}
+		this.insertStarter();
+		this.removeStarter();
 		//TODO Start the IA
 		LCD.clear();
 		startedTick = System.currentTimeMillis();
 		currentTick = System.currentTimeMillis();
-		while(!this.starter.isPressed() && currentTick-startedTick < Robot.TIME_LIMIT){
+		while(!this.ARUIsPress() && (this.timeLimit == 0 || currentTick-startedTick < this.timeLimit)){
 			currentTick = System.currentTimeMillis();
-			LCD.drawString((currentTick-startedTick)/1000 + " Secondes",0,0);
+			if(this.timeLimit != 0){
+				LCD.drawString((this.timeLimit-(currentTick-startedTick))/1000 + " Secondes",0,0);
+			}
 			Thread.yield();
 		}
 		System.out.println("STOP !!!");
 		for(Thread t : this.tasks){
 			t.interrupt();
 		}
-		if(currentTick-startedTick >= Robot.TIME_LIMIT){
+		if(this.timeLimit != 0 && currentTick-startedTick >= this.timeLimit){
 			//TODO Funny action
 		}
 		System.out.println("STOPED !!");
