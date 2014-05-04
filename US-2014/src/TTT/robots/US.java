9681c1aeed0b69bbaaa898517100a2c910fd486b
SABATIER Boris <sabatier.boris@gmail.com>
2014-05-04 15:49:52 +0200

    Implementation final du projet remote Sensor

--- US-2014/src/TTT/robots/US.java
+++ US-2014/src/TTT/robots/US.java
@@ -1,22 +1,41 @@
 package TTT.robots;
 
 import lejos.nxt.Button;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
 
 import TTT.libNXT.robot.Robot;
+
 import TTT.libNXT.communication.BluetoothConnexion;
+
+import TTT.libNXT.task.RealUSMonitor;
+import TTT.libNXT.task.ServeUSMonitor;
 import TTT.libNXT.task.WatchConnexion;
 
 public class US extends Robot{
 
 	public US(){
 		super(0l);
+
 		byte[] pin = {(byte)'0', (byte)'4', (byte)'0', (byte)'5'};
 		BluetoothConnexion bluetooth = BluetoothConnexion.getInstance();
+
 		bluetooth.setPin(pin);
 		bluetooth.setName("SENSORS");
 		bluetooth.setSlave();
+
+		RealUSMonitor usMon = new RealUSMonitor();
+		usMon.addSensor("FRONT-LEFT",new UltrasonicSensor(SensorPort.S4));
+		usMon.addSensor("FRONT-RIGHT",new UltrasonicSensor(SensorPort.S1));
+		usMon.addSensor("BACK-LEFT",new UltrasonicSensor(SensorPort.S2));
+		usMon.addSensor("BACK-RIGHT",new UltrasonicSensor(SensorPort.S3));
+		
+		ServeUSMonitor servMon = new ServeUSMonitor(usMon,bluetooth);
+
 		this.addTask(bluetooth);
 		this.addTask(new WatchConnexion(bluetooth));
+		this.addTask(usMon);
+		this.addTask(servMon);
 	}
 
 	@Override
